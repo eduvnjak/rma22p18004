4cf7e7a40a4316682ee7789f35a52b0df1159dfd
Eman <eduvnjak1@etf.unsa.ba>
2022-04-07 01:32:33 +0200

    Grupa i Istrazivanje repository sa mockup podacima i implementiranim funkcijama

--- app/src/main/java/ba/unsa/etf/rma/rma22p18004/GrupaRepository.kt
+++ app/src/main/java/ba/unsa/etf/rma/rma22p18004/GrupaRepository.kt
@@ -1,7 +1,22 @@
 package ba.unsa.etf.rma.rma22p18004
 
 object GrupaRepository {
+    val sveGrupe: List<Grupa> = arrayListOf(
+        Grupa("I1 G1","Istrazivanje 1"),
+        Grupa("I1 G2","Istrazivanje 1"),
+        Grupa("I2 G1","Istrazivanje 2"),
+        Grupa("I3 G2","Istrazivanje 3"),
+        Grupa("I3 G2","Istrazivanje 3"),
+        Grupa("I4 G1","Istrazivanje 4"),
+        Grupa("I5 G2","Istrazivanje 5"),
+    )
+    val upisaneGrupe: List<Grupa> = arrayListOf(
+        Grupa("I1 G1","Istrazivanje 1"),
+        Grupa("I1 G2","Istrazivanje 1"),
+        Grupa("I2 G1","Istrazivanje 2"),
+        )
+
     fun getGroupsByIstrazivanjet(nazivIstrazivanja:String) : List<Grupa>{
-        return emptyList()
+        return sveGrupe.filter { grupa -> grupa.nazivIstrazivanja == nazivIstrazivanja }
     }
 }
--- app/src/main/java/ba/unsa/etf/rma/rma22p18004/IstrazivanjeRepository.kt
+++ app/src/main/java/ba/unsa/etf/rma/rma22p18004/IstrazivanjeRepository.kt
@@ -1,14 +1,28 @@
 package ba.unsa.etf.rma.rma22p18004
 
 object IstrazivanjeRepository {
+    val svaIstrazivanja: List<Istrazivanje> = arrayListOf(
+        Istrazivanje("Istrazivanje 1", 1),
+        Istrazivanje("Istrazivanje 2", 1),
+        Istrazivanje("Istrazivanje 3", 1),
+        Istrazivanje("Istrazivanje 4", 2),
+        Istrazivanje("Istrazivanje 5", 2),
+        Istrazivanje("Istrazivanje 6", 3),
+    )
+    val upisanaIstrazivanja: List<Istrazivanje> = arrayListOf(
+        Istrazivanje("Istrazivanje 1", 1),
+        Istrazivanje("Istrazivanje 4", 2),
+        Istrazivanje("Istrazivanje 5", 2),
+        )
+
     fun getIstrazivanjeByGodina(godina: Int): List<Istrazivanje>{
-        return emptyList()
+        return svaIstrazivanja.filter {istrazivanje -> istrazivanje.godina == godina  }
     }
     fun getAll(): List<Istrazivanje>{
-        return emptyList()
+        return svaIstrazivanja
     }
     fun getUpisani(): List<Istrazivanje>{
-        return emptyList()
+        return upisanaIstrazivanja
     }
 }
 
