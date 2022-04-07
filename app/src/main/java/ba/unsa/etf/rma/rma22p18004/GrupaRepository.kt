e90382a75eab8209add7c87c1cca1ab448b405e8
Eman <eduvnjak1@etf.unsa.ba>
2022-04-07 15:30:59 +0200

    Grupa mockup podaci ispravka

--- app/src/main/java/ba/unsa/etf/rma/rma22p18004/GrupaRepository.kt
+++ app/src/main/java/ba/unsa/etf/rma/rma22p18004/GrupaRepository.kt
@@ -5,15 +5,16 @@ object GrupaRepository {
         Grupa("I1 G1","Istrazivanje 1"),
         Grupa("I1 G2","Istrazivanje 1"),
         Grupa("I2 G1","Istrazivanje 2"),
-        Grupa("I3 G2","Istrazivanje 3"),
+        Grupa("I3 G1","Istrazivanje 3"),
         Grupa("I3 G2","Istrazivanje 3"),
         Grupa("I4 G1","Istrazivanje 4"),
+        Grupa("I4 G2", "Istrazivanje 4"),
         Grupa("I5 G2","Istrazivanje 5"),
     )
     val upisaneGrupe: List<Grupa> = arrayListOf(
         Grupa("I1 G1","Istrazivanje 1"),
-        Grupa("I1 G2","Istrazivanje 1"),
-        Grupa("I2 G1","Istrazivanje 2"),
+        Grupa("I4 G1","Istrazivanje 4"),
+        Grupa("I5 G1","Istrazivanje 5"),
         )
 
     fun getGroupsByIstrazivanjet(nazivIstrazivanja:String) : List<Grupa>{
