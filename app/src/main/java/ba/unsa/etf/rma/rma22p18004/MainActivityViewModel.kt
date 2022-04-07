83f5b12c47644de332aa6d74a473040c8b613855
Eman <eduvnjak1@etf.unsa.ba>
2022-04-08 00:36:41 +0200

    Ankete sortirane po datumu pocetka

--- app/src/main/java/ba/unsa/etf/rma/rma22p18004/MainActivityViewModel.kt
+++ app/src/main/java/ba/unsa/etf/rma/rma22p18004/MainActivityViewModel.kt
@@ -2,25 +2,25 @@ package ba.unsa.etf.rma.rma22p18004
 
 class MainActivityViewModel {
     fun dajAnkete(): List<Anketa> {
-        return AnketaRepository.getMyAnkete()
+        return AnketaRepository.getMyAnkete().sortedBy { it.datumPocetak  }
     }
 
     fun filtriraj(odabranaOpcija: String): List<Anketa> {
         when(odabranaOpcija) {
             "Sve moje ankete" -> {
-                return AnketaRepository.getMyAnkete()
+                return AnketaRepository.getMyAnkete().sortedBy { it.datumPocetak  }
             }
             "Sve ankete" -> {
-                return AnketaRepository.getAll()
+                return AnketaRepository.getAll().sortedBy { it.datumPocetak  }
             }
             "Urađene ankete" -> {
-                return AnketaRepository.getDone()
+                return AnketaRepository.getDone().sortedBy { it.datumPocetak  }
             }
             "Buduće ankete" -> {
-                return AnketaRepository.getFuture()
+                return AnketaRepository.getFuture().sortedBy { it.datumPocetak  }
             }
             "Prošle (neurađene) ankete" -> {
-                return AnketaRepository.getNotTaken()
+                return AnketaRepository.getNotTaken().sortedBy { it.datumPocetak  }
             }
         }
         return emptyList()
