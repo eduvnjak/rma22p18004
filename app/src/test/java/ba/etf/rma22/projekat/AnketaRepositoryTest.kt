573842518503644eb2354c66587ad51daaf56a5f
Eman <eduvnjak1@etf.unsa.ba>
2022-04-13 16:29:50 +0200

    Unit testovi repository klasa

---
+++ app/src/test/java/ba/etf/rma22/projekat/AnketaRepositoryTest.kt
@@ -0,0 +1,56 @@
+package ba.etf.rma22.projekat
+
+import ba.etf.rma22.projekat.data.models.Grupa
+import ba.etf.rma22.projekat.data.repositories.AnketaRepository
+import ba.etf.rma22.projekat.data.repositories.GrupaRepository
+import org.junit.Assert
+import org.junit.Test
+
+class AnketaRepositoryTest {
+    val anketaRep = AnketaRepository
+    @Test
+    fun test1(){
+        val sveMojeAnkete = anketaRep.getMyAnkete()
+        val ostaleAnkete = anketaRep.getAll().minus(sveMojeAnkete)
+        val upisaneGrupe = GrupaRepository.upisaneGrupe
+        for (anketa in sveMojeAnkete){
+            Assert.assertTrue(upisaneGrupe.contains(Grupa(anketa.nazivGrupe, anketa.nazivIstrazivanja)))
+        }
+        for (anketa in ostaleAnkete){
+            Assert.assertFalse(upisaneGrupe.contains(Grupa(anketa.nazivGrupe, anketa.nazivIstrazivanja)))
+        }
+    }
+    @Test
+    fun test2(){
+        val uradjeneAnkete = anketaRep.getDone()
+        val sviOstali = anketaRep.getMyAnkete().minus(uradjeneAnkete)
+        for (anketa in uradjeneAnkete){
+            Assert.assertTrue(anketa.dajStatusAnkete() == 1)
+        }
+        for(anketa in sviOstali){
+            Assert.assertTrue(anketa.dajStatusAnkete() != 1)
+        }
+    }
+    @Test
+    fun test3(){
+        val buduceAnkete = anketaRep.getFuture()
+        val sviOstali = anketaRep.getMyAnkete().minus(buduceAnkete)
+        for (anketa in buduceAnkete){
+            Assert.assertTrue(anketa.dajStatusAnkete() == 3)
+        }
+        for (anketa in sviOstali){
+            Assert.assertTrue(anketa.dajStatusAnkete() != 3)
+        }
+    }
+    @Test
+    fun test4(){
+        val prosleAnkete = anketaRep.getNotTaken()
+        val sviOstali = anketaRep.getMyAnkete().minus(prosleAnkete)
+        for (anketa in prosleAnkete){
+            Assert.assertTrue(anketa.dajStatusAnkete() == 4)
+        }
+        for (anketa in sviOstali){
+            Assert.assertTrue(anketa.dajStatusAnkete() != 4)
+        }
+    }
+}
---
+++ app/src/test/java/ba/etf/rma22/projekat/IstrazivanjaGrupeTest.kt
@@ -0,0 +1,51 @@
+package ba.etf.rma22.projekat
+
+import ba.etf.rma22.projekat.data.models.Grupa
+import ba.etf.rma22.projekat.data.models.Istrazivanje
+import ba.etf.rma22.projekat.data.repositories.GrupaRepository
+import ba.etf.rma22.projekat.data.repositories.IstrazivanjeRepository
+import org.junit.Assert
+import org.junit.Test
+
+class IstrazivanjaGrupeTest {
+
+    val istrazivanjeRep = IstrazivanjeRepository
+    val grupaRep = GrupaRepository
+    @Test
+    fun testPredmetiSaGodina(){
+        val istrazivanjaSaGodine1 = istrazivanjeRep.dajIstrazivanjaNaKojaNijeUpisan(1)
+        val ocekivaniSaPrve = listOf(Istrazivanje("Istrazivanje 2",1),
+                                    Istrazivanje("Istrazivanje 3",1),)
+        Assert.assertTrue(istrazivanjaSaGodine1.size==ocekivaniSaPrve.size && istrazivanjaSaGodine1.containsAll(ocekivaniSaPrve) && ocekivaniSaPrve.containsAll(istrazivanjaSaGodine1))
+        val istrazivanjaSaGodine2 = istrazivanjeRep.dajIstrazivanjaNaKojaNijeUpisan(2)
+        Assert.assertTrue(istrazivanjaSaGodine2.isEmpty())
+        val istrazivanjaSaGodine3 = istrazivanjeRep.dajIstrazivanjaNaKojaNijeUpisan(3)
+        val ocekivaniSaTrece = listOf(Istrazivanje("Istrazivanje 6",3))
+        Assert.assertTrue(istrazivanjaSaGodine3.size == ocekivaniSaTrece.size && istrazivanjaSaGodine3.containsAll(ocekivaniSaTrece) && ocekivaniSaTrece.containsAll(istrazivanjaSaGodine3))
+        val istrazivanjaSaGodine4 = istrazivanjeRep.dajIstrazivanjaNaKojaNijeUpisan(4)
+        Assert.assertTrue(istrazivanjaSaGodine4.isEmpty())
+        val istrazivanjaSaGodine5 = istrazivanjeRep.dajIstrazivanjaNaKojaNijeUpisan(5)
+        Assert.assertTrue(istrazivanjaSaGodine5.isEmpty())
+    }
+    @Test
+    fun testGrupeSaPredmeta(){
+        val grupe1 = grupaRep.getGroupsByIstrazivanje("Istrazivanje 1")
+        val ocekivaneGrupe1 = listOf(
+            Grupa("I1 G1","Istrazivanje 1"),
+            Grupa("I1 G2","Istrazivanje 1"),
+        )
+        Assert.assertTrue(grupe1.size==ocekivaneGrupe1.size && grupe1.containsAll(ocekivaneGrupe1) && ocekivaneGrupe1.containsAll(grupe1))
+        val grupe2 = grupaRep.getGroupsByIstrazivanje("Istrazivanje 2")
+        val ocekivaneGrupe2 = listOf(
+            Grupa("I2 G1","Istrazivanje 2"),
+            Grupa("I2 G2","Istrazivanje 2")
+        )
+        Assert.assertTrue(grupe2.size==ocekivaneGrupe2.size && grupe2.containsAll(ocekivaneGrupe2) && ocekivaneGrupe2.containsAll(grupe2))
+        val grupe3 = grupaRep.getGroupsByIstrazivanje("Istrazivanje 4")
+        val ocekivaneGrupe3 = listOf(
+            Grupa("I4 G1", "Istrazivanje 4"),
+            Grupa("I4 G2", "Istrazivanje 4"),
+        )
+        Assert.assertTrue(grupe3.size==ocekivaneGrupe3.size && grupe3.containsAll(ocekivaneGrupe3) && ocekivaneGrupe3.containsAll(grupe3))
+    }
+}
